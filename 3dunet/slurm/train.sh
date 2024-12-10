95a7b4db575ec75914f29184fc68f65ae8b7354d
Tevfik Kiziloren <tevfikkiziloren@gmail.com>
2024-12-10 18:27:42 +0000

    slurm & pdb config of first version

--- 3dunet/slurm/train.sh
+++ 3dunet/slurm/train.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=pdb-training        # İş ismi
+#SBATCH --job-name=3d-unet             # İş ismi
 #SBATCH --output=logs/%x_%j.log        # Çıkış dosyası (logs dizinine kaydeder)
 #SBATCH --error=logs/%x_%j.err         # Hata dosyası
-#SBATCH --nodes=1                      # 1 node kullan
 #SBATCH --gres=gpu:4                   # 4 GPU tahsis et
 #SBATCH --ntasks=1                     # Tek görev
 #SBATCH --cpus-per-task=16             # 16 CPU çekirdeği
