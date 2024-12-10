3869c95b16118a4de2454a113e4cb67300a41677
Tevfik Kiziloren <tevfikkiziloren@gmail.com>
2024-12-10 18:39:50 +0000

    slurm & pdb config of first version

--- 3dunet/slurm/train.sh
+++ 3dunet/slurm/train.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=3d-unet             # İş ismi
-#SBATCH --output=logs/%x_%j.log        # Çıkış dosyası (logs dizinine kaydeder)
-#SBATCH --error=logs/%x_%j.err         # Hata dosyası
-#SBATCH --gres=gpu:4                   # 4 GPU tahsis et
-#SBATCH --ntasks=1                     # Tek görev
-#SBATCH --cpus-per-task=16             # 16 CPU çekirdeği
-#SBATCH --mem=64G                      # 64 GB RAM
+#SBATCH --job-name=3d-unet
+#SBATCH --output=logs/%x_%j.log
+#SBATCH --error=logs/%x_%j.err
+#SBATCH --gres=gpu:v100:1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=64G
 #SBATCH --time=7-00:00:00
-#SBATCH --mail-type=ALL                # Job bitiş ve hata durumlarında mail gönder
-#SBATCH --mail-user=tevfik             # Mail adresiniz
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=tevfik
 #SBATCH --partition=datamover
 
 # Modül yükleme (gerekiyorsa)
