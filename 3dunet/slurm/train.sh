4eaedbb098fa614193d1579d4ecc1bc198f05fa5
Tevfik Kiziloren <tevfikkiziloren@gmail.com>
2024-12-15 17:08:49 +0000

    run config change to support different configurations to hyperparameter optimization

--- 3dunet/slurm/train.sh
+++ 3dunet/slurm/train.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
-#SBATCH --job-name=3d-unet
-#SBATCH --gres=gpu:v100:1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=64G
-#SBATCH --time=7-00:00:00
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=tevfik
-# Parametreler
-LOG_PREFIX=${1:-job_output}  # Log dosyası prefix'i
-CONFIG_PATH=${2:-config/config.yml}  # Config dosyası
+# Slurm direktiflerini script dışından sağlamak için yorum olarak bırakıyoruz.
+# #SBATCH --job-name=3d-unet
+# #SBATCH --gres=gpu:v100:1
+# #SBATCH --ntasks=1
+# #SBATCH --cpus-per-task=16
+# #SBATCH --mem=64G
+# #SBATCH --time=7-00:00:00
+# #SBATCH --mail-type=ALL
+# #SBATCH --mail-user=tevfik
 
+# Dinamik parametreler
+LOG_PREFIX=${1:-job_output}
+CONFIG_PATH=${2:-config/config.yml}
+GPU_COUNT=${3:-1}
 
-cd /homes/tevfik/PHD/3dunet-apbs/3dunet
 LOG_DIR=/homes/tevfik/PHD/3dunet-apbs/slurm_run_logs
-STDOUT_LOG="${LOG_DIR}/${LOG_PREFIX}_${SLURM_JOB_ID}.out"
-STDERR_LOG="${LOG_DIR}/${LOG_PREFIX}_${SLURM_JOB_ID}.err"
+STDOUT_LOG="${LOG_DIR}/${LOG_PREFIX}_%j.out"
+STDERR_LOG="${LOG_DIR}/${LOG_PREFIX}_%j.err"
 
-echo "Config Path: $CONFIG_PATH"
-echo "Log Prefix: $LOG_PREFIX"
-echo "Stdout Log File: $STDOUT_LOG"
-echo "Stderr Log File: $STDERR_LOG"
-
-module load python/3.10.10
-source ~/PHD/3dunet-apbs/venv/bin/activate
-export PYTHONUNBUFFERED=1 PYTORCH_ENABLE_MPS_FALLBACK=1
-python main.py --config "${CONFIG_PATH}" > "${STDOUT_LOG}" 2> "${STDERR_LOG}"
-#### RUN LIKE THIS: sbatch train.sh 2 pdbbind config/codon/pdbbind.yml
+# Slurm job’u komut satırı parametreleriyle çalıştır
+sbatch --job-name=3d-unet \
+       --gres=gpu:v100:${GPU_COUNT} \
+       --ntasks=1 \
+       --cpus-per-task=16 \
+       --mem=64G \
+       --time=7-00:00:00 \
+       --mail-type=ALL \
+       --mail-user=tevfik \
+       --output="${STDOUT_LOG}" \
+       --error="${STDERR_LOG}" \
+       --wrap="module load python/3.10.10 && \
+               source ~/PHD/3dunet-apbs/venv/bin/activate && \
+               python main.py --config ${CONFIG_PATH}"
